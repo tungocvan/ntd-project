74ae7948325af6eea5b4563c508cc1a8a2459d8e
Tu Ngoc Van <tungocvan@gmail.com>
2026-04-25 07:20:26 +0700

    25042026-6

--- Modules/Admission/Livewire/Public/RegistrationForm.php
+++ Modules/Admission/Livewire/Public/RegistrationForm.php
@@ -24,130 +24,59 @@ class RegistrationForm extends Component
     public $isEdit = false;
 
     // ⚠️ QUAN TRỌNG: GIỮ NGUYÊN KEY PascalCase
-    // public $form = [
-
-    //     // STEP 1
-    //     'HoVaTenHocSinh' => '',
-    //     'GioiTinh' => '',
-    //     'NgaySinh' => '',
-    //     'DanToc' => 'Kinh',
-    //     'MaDinhDanh' => '',
-    //     'QuocTich' => 'Việt Nam',
-    //     'TonGiao' => 'Không',
-    //     'SDTEnetViet' => '',
-    //     'NoiSinh' => '',
-    //     'NoiDangKyKhaiSinh' => '',
-    //     'QueQuan' => '',
-
-    //     // STEP 2
-    //     'TTSN' => '',
-    //     'TTD' => '',
-    //     'TTKP' => '',
-    //     'TTPX' => '',
-    //     'TTTTP' => '',
-
-    //     'HTSN' => '',
-    //     'HTD' => '',
-    //     'HTKP' => '',
-    //     'HTPX' => '',
-    //     'HTTTP' => '',
-
-    //     // STEP 3
-    //     'OChungVoi' => '',
-    //     'QuanHeNguoiNuoiDuong' => '',
-    //     'ConThu' => '',
-    //     'TSAnhChiEm' => '',
-    //     'HoanThanhLopLa' => '',
-    //     'TruongMamNon' => '',
-    //     'KhaNangHocSinh' => [],
-    //     'SucKhoeCanLuuY' => [],
-    //     'SucKhoeKhac' => '',
-
-    //     // STEP 4
-    //     'HoTenCha' => '',
-    //     'NamSinhCha' => '',
-    //     'NgheNghiepCha' => '',
-    //     'ChucVuCha' => '',
-    //     'DienThoaiCha' => '',
-    //     'CCCDCha' => '',
-
-    //     'HoTenMe' => '',
-    //     'NamSinhMe' => '',
-    //     'NgheNghiepMe' => '',
-    //     'ChucVuMe' => '',
-    //     'DienThoaiMe' => '',
-    //     'CCCDMe' => '',
-
-    //     'HoTenNguoiGiamHo' => '',
-    //     'QuanHeGiamHo' => '',
-    //     'DienThoaiGiamHo' => '',
-    //     'CCCDGiamHo' => '',
-
-    //     // STEP 5
-    //     'LoaiLopDangKy' => '',
-    //     'CK_GocHocTap' => false,
-    //     'CK_SachVo' => false,
-    //     'CK_HopPH' => false,
-    //     'CK_ThamGiaHD' => false,
-    //     'CK_GanGui' => false,
-
-    //     'NgayLamDon' => '',
-    //     'NguoiLamDon' => '',
-    // ];
     public $form = [
 
         // STEP 1
-        'HoVaTenHocSinh' => 'Nguyễn Minh An',
-        'GioiTinh' => 'Nam',
-        'NgaySinh' => '2020-01-01',
+        'HoVaTenHocSinh' => '',
+        'GioiTinh' => '',
+        'NgaySinh' => '',
         'DanToc' => 'Kinh',
-        'MaDinhDanh' => '079120000001',
+        'MaDinhDanh' => '',
         'QuocTich' => 'Việt Nam',
         'TonGiao' => 'Không',
-        'SDTEnetViet' => '0908123456',
-        'NoiSinh' => 'Tp Hồ Chí Minh',
-        'NoiDangKyKhaiSinh' => 'UBND Quận 7',
-        'QueQuan' => 'Tp Hồ Chí Minh',
+        'SDTEnetViet' => '',
+        'NoiSinh' => '',
+        'NoiDangKyKhaiSinh' => '',
+        'QueQuan' => '',
 
         // STEP 2
-        'TTSN' => '45',
-        'TTD' => 'Huỳnh Tấn Phát',
-        'TTKP' => 'KP 2',
+        'TTSN' => '',
+        'TTD' => '',
+        'TTKP' => '',
         'TTPX' => '',
         'TTTTP' => '',
 
-        'HTSN' => '45',
-        'HTD' => 'Huỳnh Tấn Phát',
-        'HTKP' => 'KP 2',
+        'HTSN' => '',
+        'HTD' => '',
+        'HTKP' => '',
         'HTPX' => '',
         'HTTTP' => '',
 
         // STEP 3
-        'OChungVoi' => 'Cha mẹ',
+        'OChungVoi' => '',
         'QuanHeNguoiNuoiDuong' => '',
-        'ConThu' => '1',
-        'TSAnhChiEm' => '2',
-        'HoanThanhLopLa' => 'Có',
-        'TruongMamNon' => 'Rạng Đông',
+        'ConThu' => '',
+        'TSAnhChiEm' => '',
+        'HoanThanhLopLa' => '',
+        'TruongMamNon' => '',
         'KhaNangHocSinh' => [],
         'SucKhoeCanLuuY' => [],
         'SucKhoeKhac' => '',
 
         // STEP 4
-        'HoTenCha' => 'Nguyễn Văn Hùng',
-        'NamSinhCha' => '1990',
-        'NgheNghiepCha' => 'Kỹ sư',
+        'HoTenCha' => '',
+        'NamSinhCha' => '',
+        'NgheNghiepCha' => '',
         'ChucVuCha' => '',
-        'DienThoaiCha' => '0909000001',
-        'CCCDCha' => '079088880001
-',
+        'DienThoaiCha' => '',
+        'CCCDCha' => '',
 
-        'HoTenMe' => 'Trần Thị Mai',
-        'NamSinhMe' => '1992',
-        'NgheNghiepMe' => 'Kinh doanh',
+        'HoTenMe' => '',
+        'NamSinhMe' => '',
+        'NgheNghiepMe' => '',
         'ChucVuMe' => '',
-        'DienThoaiMe' => '0909000002',
-        'CCCDMe' => '079088880002',
+        'DienThoaiMe' => '',
+        'CCCDMe' => '',
 
         'HoTenNguoiGiamHo' => '',
         'QuanHeGiamHo' => '',
@@ -155,16 +84,88 @@ class RegistrationForm extends Component
         'CCCDGiamHo' => '',
 
         // STEP 5
-        'LoaiLopDangKy' => 'Tích hợp',
-        'CK_GocHocTap' => true,
-        'CK_SachVo' => true,
-        'CK_HopPH' => true,
-        'CK_ThamGiaHD' => true,
-        'CK_GanGui' => true,
+        'LoaiLopDangKy' => '',
+        'CK_GocHocTap' => false,
+        'CK_SachVo' => false,
+        'CK_HopPH' => false,
+        'CK_ThamGiaHD' => false,
+        'CK_GanGui' => false,
 
         'NgayLamDon' => '',
-        'NguoiLamDon' => 'Trần Thị Mai',
+        'NguoiLamDon' => '',
     ];
+
+//     public $form = [
+//         // STEP 1
+//         'HoVaTenHocSinh' => 'Nguyễn Minh An',
+//         'GioiTinh' => 'Nam',
+//         'NgaySinh' => '2020-01-01',
+//         'DanToc' => 'Kinh',
+//         'MaDinhDanh' => '079120000001',
+//         'QuocTich' => 'Việt Nam',
+//         'TonGiao' => 'Không',
+//         'SDTEnetViet' => '0908123456',
+//         'NoiSinh' => 'Tp Hồ Chí Minh',
+//         'NoiDangKyKhaiSinh' => 'UBND Quận 7',
+//         'QueQuan' => 'Tp Hồ Chí Minh',
+
+//         // STEP 2
+//         'TTSN' => '45',
+//         'TTD' => 'Huỳnh Tấn Phát',
+//         'TTKP' => 'KP 2',
+//         'TTPX' => '',
+//         'TTTTP' => '',
+
+//         'HTSN' => '45',
+//         'HTD' => 'Huỳnh Tấn Phát',
+//         'HTKP' => 'KP 2',
+//         'HTPX' => '',
+//         'HTTTP' => '',
+
+//         // STEP 3
+//         'OChungVoi' => 'Cha mẹ',
+//         'QuanHeNguoiNuoiDuong' => '',
+//         'ConThu' => '1',
+//         'TSAnhChiEm' => '2',
+//         'HoanThanhLopLa' => 'Có',
+//         'TruongMamNon' => 'Rạng Đông',
+//         'KhaNangHocSinh' => [],
+//         'SucKhoeCanLuuY' => [],
+//         'SucKhoeKhac' => '',
+
+//         // STEP 4
+//         'HoTenCha' => 'Nguyễn Văn Hùng',
+//         'NamSinhCha' => '1990',
+//         'NgheNghiepCha' => 'Kỹ sư',
+//         'ChucVuCha' => '',
+//         'DienThoaiCha' => '0909000001',
+//         'CCCDCha' => '079088880001
+// ',
+
+//         'HoTenMe' => 'Trần Thị Mai',
+//         'NamSinhMe' => '1992',
+//         'NgheNghiepMe' => 'Kinh doanh',
+//         'ChucVuMe' => '',
+//         'DienThoaiMe' => '0909000002',
+//         'CCCDMe' => '079088880002',
+
+//         'HoTenNguoiGiamHo' => '',
+//         'QuanHeGiamHo' => '',
+//         'DienThoaiGiamHo' => '',
+//         'CCCDGiamHo' => '',
+
+//         // STEP 5
+//         'LoaiLopDangKy' => 'Tích hợp',
+//         'CK_GocHocTap' => true,
+//         'CK_SachVo' => true,
+//         'CK_HopPH' => true,
+//         'CK_ThamGiaHD' => true,
+//         'CK_GanGui' => true,
+
+//         'NgayLamDon' => '',
+//         'NguoiLamDon' => 'Trần Thị Mai',
+//     ];
+
     protected $rules = [
         'form.HoVaTenHocSinh' => 'required|min:5',
         'form.MaDinhDanh' => 'required|digits:12',
@@ -184,7 +185,7 @@ class RegistrationForm extends Component
     public function mount($id = null)
     {
         // Load danh mục (giữ nguyên)
-              $this->provinces = AdmissionLocation::select('province_name')->distinct()->get()->toArray();
+        $this->provinces = AdmissionLocation::select('province_name')->distinct()->get()->toArray();
         $this->ethnicities = AdmissionCatalog::where('type', 'ethnicity')->get()->toArray();
         $this->religions = AdmissionCatalog::where('type', 'religion')->get()->toArray();
 
@@ -423,8 +424,6 @@ class RegistrationForm extends Component
                     'redirectUrl' => route('admission.register')
                 ]);
             }
-
-
         } catch (\Exception $e) {
             \Log::error("Lỗi lưu đơn: " . $e->getMessage());
         }
